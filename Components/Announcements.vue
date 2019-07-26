cc8c47c32d4c230206167c6985e842ee9db9a60e
Muah <ctf0@users.noreply.github.com>
2019-07-26 12:32:08 +0200

    Update Announcements.vue

--- Components/Announcements.vue
+++ Components/Announcements.vue
@@ -11,8 +11,12 @@ export default {
             notifications: []
         }
     },
-    firebase: {
-        notifications: db.ref(process.env.MIX_FB_COLLECTION_NAME)
+    beforeMount() {
+        db.ref(process.env.MIX_FB_COLLECTION_NAME).orderByChild('timestamp').startAt(Date.now()).on('child_added', (snapshot) => {
+            let notifs = this.notifications
+
+            notifs.length ? notifs.unshift(snapshot.val()) : notifs.push(snapshot.val())
+        })
     }
 }
 </script>
